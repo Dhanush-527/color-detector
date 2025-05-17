05c6c4db38e9922849905b81b14077cfcd85d54e
Dhanush-527 <dhanush007yt@gmail.com>
2025-05-17 02:53:20 -0700

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_color_name(R, G, B, color_data):
     for _, row in color_data.iterrows():
         try:
           d = ((R - int(row['R']))**2 + (G - int(row['G']))**2 + (B - int(row['B']))**2) * 0.5  # Euclidean distance
-            if d < min_dist:
+          if d < min_dist:
                 min_dist = d
                 closest_color = row
         except Exception as e:
